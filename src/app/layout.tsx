70e435c33c378050f86f92773cca8a6dc67815dd
hitesh <hp5741609@gmail.com>
2026-05-16 01:42:58 +0530

    style: adjust global typography tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col antialiased tracking-tight">
         <ClerkProvider>
           <MainHeader />
           {children}
